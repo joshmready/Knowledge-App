364be058a94695ff61942aaf63a4790088f406a2
Josh Ready <jready9767@gmail.com>
2024-03-07 19:22:10 -0600

    added react navigation Paper

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import 'react-native-gesture-handler';
 import Navigation from './src/Navigation';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <Navigation />
+      <PaperProvider>
+        <SafeAreaProvider>
+          <StatusBar barStyle="dark-content" />
+          <Navigation />
+        </SafeAreaProvider>
+      </PaperProvider>
     </>
   );
 }
